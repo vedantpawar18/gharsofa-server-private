9810adeaf6e10a6fee6ec3aec84c251fbd2a1f4d
Vedant Pawar <101568818+vedantpawar18@users.noreply.github.com>
2025-04-03 12:31:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: true, // Allow all origins
-    credentials: true,
+    origin: (origin, callback) => {
+      callback(null, true); 
+    },
+    credentials: true,  
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: [
       "Content-Type",
